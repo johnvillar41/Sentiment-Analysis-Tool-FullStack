65e8129d916a675896b73e39117404253f5b53c2
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-21 19:57:19 +0800

    Added CorpusRecordService

---
+++ SentimentAnalysisTool.Services/Implementations/CorpusRecordService.cs
@@ -0,0 +1,19 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Implementations
+{
+    public class CorpusRecordService : ICorpusRecordService
+    {
+        public Task<bool> AddCorpusRecordAsync(CorpusRecordModel corpus, string baseUrl, HttpClient httpClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Services/Interfaces/CorpusRecordService.cs
@@ -0,0 +1,15 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Interfaces
+{
+    public interface ICorpusRecordService
+    {
+        Task<bool> AddCorpusRecordAsync(CorpusRecordModel corpus, string baseUrl, HttpClient httpClient);
+    }
+}
