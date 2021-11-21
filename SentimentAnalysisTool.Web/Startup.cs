f36533b6974c61d9495ce425a44f608bebe1a8b6
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-21 14:07:44 +0800

    Added Apikey Attribute on appsettings.json and updated startup.cs

--- SentimentAnalysisTool.Web/Startup.cs
+++ SentimentAnalysisTool.Web/Startup.cs
@@ -24,6 +24,11 @@ namespace SentimentAnalysisTool.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient("SentimentAnalysisTool.Api", m =>
+            {
+                m.BaseAddress = new Uri("");
+                m.DefaultRequestHeaders.Add("Apikey", Configuration.GetValue<string>("Apikey"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
