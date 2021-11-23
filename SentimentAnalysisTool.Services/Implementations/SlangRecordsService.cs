a2d9a22d6fbf4aa78e5d83187b76c52dcdd80c44
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-23 09:22:14 +0800

    Implemented SlangRecordsService

---
+++ SentimentAnalysisTool.Services/Implementations/SlangRecordsService.cs
@@ -0,0 +1,47 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Implementations
+{
+    public class SlangRecordsService : ISlangRecordsService
+    {
+        public async Task<bool> AddSlangRecordAsync(SlangRecordModel slangRecord, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.PostAsJsonAsync<SlangRecordModel>($"{baseUrl}/api/SlangRecords", slangRecord);
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+
+        public async Task<bool> AddSlangRecordsAsync(IEnumerable<SlangRecordModel> slangRecords, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.PostAsJsonAsync<IEnumerable<SlangRecordModel>>($"{baseUrl}/api/SlangRecords", slangRecords);
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+
+        public async Task<bool> DeleteSlangRecordAsync(int slangRecordId, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.DeleteAsync($"{baseUrl}/api/SlangRecords/{slangRecordId}");
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Services/Interfaces/ISlangRecordsService.cs
@@ -0,0 +1,17 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Interfaces
+{
+    public interface ISlangRecordsService
+    {
+        Task<bool> AddSlangRecordAsync(SlangRecordModel slangRecord, string baseUrl, HttpClient httpClient);
+        Task<bool> AddSlangRecordsAsync(IEnumerable<SlangRecordModel> slangRecords, string baseUrl, HttpClient httpClient);
+        Task<bool> DeleteSlangRecordAsync(int slangRecordId, string baseUrl, HttpClient httpClient);
+    }
+}
