1a4a9b3fc11e0dc2258c805108b5cfe08ca89ce5
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-23 09:15:16 +0800

    Implemented RecordsService

---
+++ SentimentAnalysisTool.Services/Implementations/RecordsService.cs
@@ -0,0 +1,36 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Implementations
+{
+    public class RecordsService : IRecordsService
+    {
+        public async Task<bool> AddRecordAsync(RecordModel record, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.PostAsJsonAsync<RecordModel>($"{baseUrl}/api/Records", record);
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+
+        public async Task<bool> DeleteRecordAsync(int recordId, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.DeleteAsync($"{baseUrl}/api/Records/{recordId}");
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Services/Interfaces/IRecordsService.cs
@@ -0,0 +1,16 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Interfaces
+{
+    public interface IRecordsService
+    {
+        Task<bool> AddRecordAsync(RecordModel record, string baseUrl, HttpClient httpClient);
+        Task<bool> DeleteRecordAsync(int recordId, string baseUrl, HttpClient httpClient);
+    }
+}
