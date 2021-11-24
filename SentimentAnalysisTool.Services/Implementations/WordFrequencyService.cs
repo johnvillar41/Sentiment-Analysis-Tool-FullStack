bec6a52d750e8282c22bc3a6a1d5db901365881e
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-24 09:57:40 +0800

    Added WordFrequencyService

---
+++ SentimentAnalysisTool.Services/Implementations/WordFrequencyService.cs
@@ -0,0 +1,36 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Implementations
+{
+    public class WordFrequencyService : IWordFrequencyService
+    {
+        public async Task<bool> AddWordFrequencyAsync(IEnumerable<WordFrequencyModel> wordFrequencies, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.PostAsJsonAsync<IEnumerable<WordFrequencyModel>>($"{baseUrl}/api/WordFrequency", wordFrequencies);
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+
+        public async Task<bool> AddWordFrequencyAsync(WordFrequencyModel wordFrequency, string baseUrl, HttpClient httpClient)
+        {
+            var response = await httpClient.PostAsJsonAsync<WordFrequencyModel>($"{baseUrl}/api/WordFrequency", wordFrequency);
+            var responseCode = response.IsSuccessStatusCode;
+
+            if (responseCode)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Services/Interfaces/IWordFrequencyService.cs
@@ -0,0 +1,16 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Interfaces
+{
+    public interface IWordFrequencyService
+    {
+        Task<bool> AddWordFrequencyAsync(IEnumerable<WordFrequencyModel> wordFrequencies, string baseUrl, HttpClient httpClient);
+        Task<bool> AddWordFrequencyAsync(WordFrequencyModel wordFrequency, string baseUrl, HttpClient httpClient);
+    }
+}
