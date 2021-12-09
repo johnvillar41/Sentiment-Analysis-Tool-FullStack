f67ed8c16460ca12dc4ea8d7e73df301be4d96c1
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-09 08:49:56 +0800

    Updated error handling on client side

---
+++ SentimentAnalysisTool.Web/wwwroot/js/records-index.js
@@ -0,0 +1,26 @@
+﻿$('#customfile').on('change', function () {
+    console.log("I am inside upload event");
+    files = $(this)[0].files;
+    name = '';
+    for (var i = 0; i < files.length; i++) {
+        name += '\"' + files[i].name + '\"' + (i != files.length - 1 ? ", " : "");
+    }
+    $(".custom-file-label").html(name);
+});
+$('#customfile').change(function () {
+    var fileExtension = ['xlsx', 'csv'];
+    if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+        $('#warningMessage').show();
+        $('#customfile').val("");
+    } else {
+        $('#warningMessage').hide();
+    }
+});
+
+promptNoNetwork = function (response) {
+    $('#errorNetwork').show();
+}
+
+promptHasNetwork = function (response) {
+    $('#errorNetwork').hide();
+}
