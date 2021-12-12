719d14479b3b2ab01501134b4b1ff5b537601c5b
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-12 19:36:16 +0800

    Updated design for uploading files

---
+++ SentimentAnalysisTool.Web/Enums/DataCorpusType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Web.Enums
+{
+    /// <summary>
+    /// This is only a temporary enum, This should be removed
+    /// once the database connection is completely implemented
+    /// </summary>
+    public enum DataCorpusType
+    {
+        FilmReview,
+        ProductReview,
+        EnvironmentReview
+    }
+}
