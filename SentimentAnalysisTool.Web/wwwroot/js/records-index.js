fcb025209a568b78fe91bb03af88d9e57c5328a7
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-12 16:30:15 +0800

    Added Sweetalert

--- SentimentAnalysisTool.Web/wwwroot/js/records-index.js
+++ SentimentAnalysisTool.Web/wwwroot/js/records-index.js
@@ -8,6 +8,7 @@
         }
         $(".custom-file-label").html(name);
     });
+
     $('#customfile').change(function () {
         var fileExtension = ['xlsx', 'csv'];
         if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
@@ -19,10 +20,78 @@
     });
 
     promptNoNetwork = function (response) {
-        $('#errorNetwork').show();
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Cannot connect to server! Please try again later..',
+        });
     }
     promptHasNetwork = function (response) {
-        $('#errorNetwork').hide();
+        Swal.fire({
+            icon: 'success',
+            title: 'Succesfull!',
+            text: 'Successfully loaded data!',
+        });
+    }
+
+    renderReviewClassification = function (response) {
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Positive', 'Negative'],
+                datasets: [{
+                    label: 'Review Classification',
+                    data: [response.reviewClassification.positivePercent, response.reviewClassification.negativePercent],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    renderSentimentOverTime = function (response) {
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Positive', 'Negative'],
+                datasets: [{
+                    label: 'Review Classification',
+                    data: [response.reviewClassification.positivePercent, response.reviewClassification.negativePercent],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     }
 });
 
