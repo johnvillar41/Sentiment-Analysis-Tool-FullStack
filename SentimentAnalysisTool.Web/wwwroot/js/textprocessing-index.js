7d0f76f451e16f8636ddfc64ab60fb22fdfa45ed
johnvillar41 <johndanielvillar123@gmail.com>
2022-02-12 13:08:56 +0800

    Finished implementing uploading of corpusWords

--- SentimentAnalysisTool.Web/wwwroot/js/textprocessing-index.js
+++ SentimentAnalysisTool.Web/wwwroot/js/textprocessing-index.js
@@ -37,8 +37,25 @@
             $('#warningMessage').hide();
         }
     });
-    //Corpus words TODO
-
+    //Corpus words
+    $('#corpus-record-file').on('change', function () {
+        console.log("I am inside upload event");
+        files = $(this)[0].files;
+        name = '';
+        for (var i = 0; i < files.length; i++) {
+            name += '\"' + files[i].name + '\"' + (i != files.length - 1 ? ", " : "");
+        }
+        $(".custom-file-label").html(name);
+    });
+    $('#corpus-record-file').change(function () {
+        var fileExtension = ['txt'];
+        if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+            $('#warningMessage').show();
+            $('#abbrev-upload-file').val("");
+        } else {
+            $('#warningMessage').hide();
+        }
+    });
     displaySwalSuccessUpload = function (message) {
         Swal.fire(
             message,
